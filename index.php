4bcb7ca779ea40ce26981af633d13f8ad569c0ee
Alaya95 <rozetta.95@mail.ru>
2021-01-25 11:13:50 +0300

    удаление и перенос данных в файлы соответствующих номерам лекций

--- index.php
+++ index.php
@@ -1,36 +1,25 @@
 <?php
 
-require "Item.php";
-require "BasketItem.php";
-require "OrderItem.php";
-require "interfaces/ProductsInterface.php";
-require "Products.php";
-require "DigitalProduct.php";
-require "PieceProduct.php";
-require "WeightProduct.php";
+require "vendor/autoload.php";
 
-// вызов класса и передаваемые параметры
-//массив  с вызовом класса и передаваемыми данными бренда и цены
+include "dbConnect.php";
 
-$items = [
-    new Item(1, "mrGrinch", "t-short", "man", 1000),
-    new BasketItem(2, "Santa", "jeans", "women", 1000, 2),
-    new OrderItem(2, "Black", "dress", "women", 1000, 2, 1),
-];
-//обходим массив с данными
-foreach ($items as $item) {
-    $item->about();
+$pathToImg = "src/img/";
+
+$result = mysqli_query($db, "SELECT * FROM image_data;");
+
+$imgArr = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $imgArr[] = [
+       "name" => $pathToImg . $row['name'],
+        "altername" => $row['altername'],
+    ];
 }
 
-//абстрактный класс.
-$piecePrice = 100;
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader);
 
-$products = [
-    new PieceProduct('Молоток', $piecePrice, 10),
-    new DigitalProduct('Лицензия', 500),
-    new WeightProduct('Картофель', $piecePrice, 0.2),
-];
-
-foreach ($products as $product) {
-    echo "<br>" . $product->getName() . ' price ' . $product->getFinalPrice();
-}
+echo $twig->render('index.twig', [
+    "images" => $imgArr,
+]);
