bba471790a6bad56daea78f35075771a8ed04655
Alaya95 <rozetta.95@mail.ru>
2021-01-28 11:18:12 +0300

    hw4

--- hw4/index.php
+++ hw4/index.php
@@ -2,17 +2,23 @@
 require "DBConnect.php";
 require "../vendor/autoload.php";
 
-$items = DBConnect::getInstance()->getAllData(DBConnect::TABLE_GOODS);
-
-if(isset($_GET['showMore'])){
-    $showMore = DBConnect::getInstance()->getShowMore(DBConnect::TABLE_GOODS);
-}
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('index.twig', [
-    "items" => $items,
-    'itemsShow' => $showMore,
-]);
+$items = $showMore = null;
+
+if(isset($_GET['showMore'])){
+    print_r($_GET);
+    $showMore = DBConnect::getInstance()->getShowMore(DBConnect::TABLE_GOODS);
+    echo $twig->render('goods.twig', [
+        "items" => $items,
+        "showMore" => $showMore,
+    ]);
+} else {
+    $items = DBConnect::getInstance()->getAllData(DBConnect::TABLE_GOODS);
+      echo $twig->render('index.twig', [
+          "items" => $items,
+      ]);
+}
 
